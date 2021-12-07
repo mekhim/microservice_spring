9bb39d372cc6b54e3ed985ff7b7690b0eec6e634
MaximeChone <31918084+MaximeChone@users.noreply.github.com>
2021-12-07 16:14:41 +0100

    config

---
+++ configservice/src/test/java/spring/configservice/ConfigserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
